7d3018b02c9886aff88416df42a0eefff4bfe987
Javier Parada <javierparada@pm.me>
2026-03-22 14:28:35 +0900

    Add SortedArrayList implementation with insertion logic

---
+++ src/lambda/collections/sorted-array-list/sorted-array-list.ts
@@ -0,0 +1,51 @@
+import { ArrayList } from "../../../core/collections/array-list/array-list";
+
+export type Comparator<T> = (left: T, right: T) => number;
+
+export class SortedArrayList<T> extends ArrayList<T> {
+  private readonly comparator: Comparator<T>;
+
+  constructor(comparator: Comparator<T>, initialElements: T[] = []) {
+    super();
+    this.comparator = comparator;
+
+    for (const element of initialElements) {
+      this.add(element);
+    }
+  }
+
+  override add(element: T): boolean {
+    const insertionIndex = this.findInsertionIndex(element);
+    super.addAt(insertionIndex, element);
+    return true;
+  }
+
+  override addAt(_index: number, element: T): void {
+    this.add(element);
+  }
+
+  override set(index: number, element: T): T {
+    const previous = super.removeAt(index);
+    this.add(element);
+    return previous;
+  }
+
+  private findInsertionIndex(element: T): number {
+    let low = 0;
+    let high = this.size();
+
+    while (low < high) {
+      const mid = low + Math.floor((high - low) / 2);
+      const middleElement = this.get(mid);
+      const comparison = this.comparator(element, middleElement);
+
+      if (comparison < 0) {
+        high = mid;
+      } else {
+        low = mid + 1;
+      }
+    }
+
+    return low;
+  }
+}
